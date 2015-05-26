8f9f1c8788780a29859230080e864c614127879a
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-05-26 08:54:36 +0300

    Add license

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Shopware 4, 5
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 require_once 'vendor/nosto/php-sdk/src/config.inc.php';
 
@@ -49,7 +71,7 @@ class Shopware_Plugins_Frontend_NostoTagging_Bootstrap extends Shopware_Componen
 	 */
 	public function getVersion()
 	{
-		return '1.0.0';
+		return '0.1.0';
 	}
 
 	/**
@@ -258,7 +280,7 @@ class Shopware_Plugins_Frontend_NostoTagging_Bootstrap extends Shopware_Componen
 
 		$view = $args->getSubject()->View();
 		$view->addTemplateDir($this->Path().'Views/');
-		$view->extendsTemplate('frontend/plugins/nosto_tagging/search/defaultSearch.tpl');
+		$view->extendsTemplate('frontend/plugins/nosto_tagging/search/index.tpl');
 
 		$this->addSearchTagging($view);
 	}
--- Views/frontend/plugins/nosto_tagging/search/index.tpl
+++ Views/frontend/plugins/nosto_tagging/search/index.tpl
@@ -21,8 +21,13 @@
 * our trademarks remain entirely with us.
 *}
 
+{block name="frontend_index_content" prepend}
+    <div class="nosto_element" id="nosto-page-search1"></div>
+{/block}
+
 {block name="frontend_index_content" append}
-    <div class="nosto_element" id="nosto-page-cart1"></div>
-    <div class="nosto_element" id="nosto-page-cart3"></div>
-    <div class="nosto_element" id="nosto-page-cart2"></div>
+    <div class="nosto_element" id="nosto-page-search2"></div>
+	{if isset($nostoSearch) && is_object($nostoSearch)}
+		<div class="nosto_search_term" style="display:none">{$nostoSearch->getSearchTerm()|escape:'htmlall':'UTF-8'}</div>
+	{/if}
 {/block}
