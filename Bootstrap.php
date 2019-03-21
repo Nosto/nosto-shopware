b055c63d489efc0f57b152272a1a58f655090585
Cid Lopes <alannettto@gmail.com>
2019-03-21 12:03:38 +0200

    Test Version Number

--- Bootstrap.php
+++ Bootstrap.php
@@ -86,7 +86,7 @@ use Nosto\Nosto;
 class Shopware_Plugins_Frontend_NostoTagging_Bootstrap extends Shopware_Components_Plugin_Bootstrap
 {
     const PLATFORM_NAME = 'shopware';
-    const PLUGIN_VERSION = '2.3.2';
+    const PLUGIN_VERSION = '9.9.9';
     const MENU_PARENT_ID = 23;  // Configuration
     const NEW_ENTITY_MANAGER_VERSION = '5.0.0';
     const NEW_ATTRIBUTE_MANAGER_VERSION = '5.2.0';
